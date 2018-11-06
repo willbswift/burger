041cae6f5d10e87505355b9e63692cf1d022db89
William B. Swift <willbswift@hotmail.com>
2018-11-06 14:06:32 -0600

    update

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,7 +4,16 @@ INSERT INTO burgers_db.burgers (burger_name)
 VALUE ("Bantha Biscuit");
 
 INSERT INTO burgers_db.burgers (burger_name)
-VALUE ("Dobac and Cheese");
+VALUE ("Biscuit Baron Royale with Cheese");
 
 INSERT INTO burgers_db.burgers (burger_name)
-VALUE ("Biscuit Baron Special");
+VALUE ("Nerf Burger");
+
+INSERT INTO burgers_db.burgers (burger_name)
+VALUE ("Double Talaxian with Cheese");
+
+INSERT INTO burgers_db.burgers (burger_name)
+VALUE ("Greasy Neelix");
+
+INSERT INTO burgers_db.burgers (burger_name)
+VALUE ("Kronkburger");
