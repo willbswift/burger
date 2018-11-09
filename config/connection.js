38a5974f394c1a7bb18efb93862e0e0ba4040ec3
William B. Swift <willbswift@hotmail.com>
2018-11-09 14:50:51 -0600

    Heroku prep

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 // Set up MySQL connection.
 let mysql = require("mysql");
+let connection
 
-let connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: 3306,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else { 
+  connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: "burgers_db"
+  });
+};
 
 // Make connection.
 connection.connect(function(err) {
